00f48da91cf4e6bf32b7220505cba86f58c37082
Ryder Stanley <ryderstanley2007@gmail.com>
2020-08-30 15:33:24 -0600

    prints out "HELLO"

--- src/boot.asm
+++ src/boot.asm
@@ -1,3 +1,23 @@
+mov ah, 0x0e
+	mov al, 'H'
+	int 0x10
+	
+	mov ah, 0x0e
+	mov al, 'E'
+	int 0x10
+	
+	mov ah, 0x0e
+	mov al, 'L'
+	int 0x10
+	
+	mov ah, 0x0e
+	mov al, 'L'
+	int 0x10
+	
+	mov ah, 0x0e
+	mov al, 'O'
+	int 0x10
+
 jmp $
 	times 510-($-$$) db 0
 	db 0x55, 0xaa
